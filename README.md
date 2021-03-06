0fc3c8adfb5a373fb0e44a2e6c6098d04efb5a3b
Matthew Blode <m@blode.co>
2021-03-06 13:36:56 +1100

    Push

---
+++ README.md
@@ -0,0 +1,45 @@
+<h1 align="center">TM</h1>
+<div align="center">
+ <strong>
+    Terminal window manager written in Rust
+ </strong>
+</div>
+
+<br />
+
+<div align="center">
+  <!-- Crates version -->
+  <a href="https://crates.io/crates/tm">
+    <img src="https://img.shields.io/crates/v/tm.svg?style=flat-square"
+    alt="Crates.io version" />
+  </a>
+  <!-- Downloads -->
+  <a href="https://crates.io/crates/tm">
+    <img src="https://img.shields.io/crates/d/tm.svg?style=flat-square"
+      alt="Download" />
+  </a>
+  <!-- docs.rs docs -->
+  <a href="https://docs.rs/tm">
+    <img src="https://img.shields.io/badge/docs-latest-blue.svg?style=flat-square"
+      alt="docs.rs docs" />
+  </a>
+</div>
+
+<div align="center">
+  <h3>
+    <a href="https://docs.rs/tm">
+      API Docs
+    </a>
+    <span> | </span>
+    <a href="https://github.com/http-rs/tm/blob/main/CONTRIBUTING.md">
+      Contributing
+    </a>
+    <span> | </span>
+    <a href="https://github.com/mblode/tm">
+      Repo
+    </a>
+  </h3>
+</div>
+
+TM is a text editor built in Rust. It’s a re-implementation of [kilo](http://antirez.com/news/108) in Rust, as outlined in this [fantastic tutorial](https://viewsourcecode.org/snaptoken/kilo/index.html). Same as the original booklet, these blog posts guide you through all the steps to build a basic text editor, Tm.
+
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,80 @@
-// mod cli;
+mod cli;
 
-// use std::io::{Read, Write, Error};
-// use std::env;
-// use std::fs::File;
-
-use termion::screen::AlternateScreen;
-use std::io::{Write, stdout};
+use std::io::{stdout, Write, Error, Stdout};
+use crossterm::{
+    QueueableCommand,
+    cursor::{
+        Show,
+        Hide,
+        MoveTo,
+    },
+    event::{read, Event, KeyCode, KeyEvent, KeyModifiers},
+    style::{Colorize, Print, PrintStyledContent},
+    terminal::{
+        EnterAlternateScreen, 
+        LeaveAlternateScreen,
+        enable_raw_mode,
+        disable_raw_mode,
+        Clear,
+        ClearType,
+        size,
+    }
+};
 
 fn main() {
-    // let matches = cli::build_cli().get_matches();
+    let matches = cli::build_cli().get_matches();
 
-    // if let Some(i) = matches.value_of("SESSION") {
-    //     let current_dir = env::current_dir().unwrap();
-    //     println!("Value for input: {}", i);
-    //     println!("Current dir: {:#?}", current_dir);
-    //     println!("Terminal size: {:#?}", termion::terminal_size());
-    //     println!("Terminal size: {:#?}", termion::terminal_size());
-    //     println!("Terminal size: {:#?}", termion::terminal_size());
-    //     println!("Terminal size: {:#?}", termion::terminal_size());
-    // }
-
-    {
-        let mut screen = AlternateScreen::from(stdout());
-        write!(screen, "Writing to alternate screen!").unwrap();
-        screen.flush().unwrap();
+    if let Some(i) = matches.value_of("SESSION") {
+        println!("Session: {}", i);
+        run(); 
     }
-    println!("Writing to main screen.");
+}
 
+fn run () {
+    let mut stdout = stdout();
+
+    enable_raw_mode().expect("failed to enable raw mode");
+
+    stdout
+        .queue(Hide).unwrap()
+        .queue(EnterAlternateScreen).unwrap()
+        .queue(Clear(ClearType::All)).unwrap();
+
+    stdout.flush().unwrap();
+
+    let terminal_size = crossterm::terminal::size().unwrap();
+
+    loop {
+
+        draw(&mut stdout, width / 2, height / 2).unwrap();
+        draw(&mut stdout, width / 2, height / 2).unwrap();
+        draw(&mut stdout, width / 2, height).unwrap();
+
+        match read().unwrap() {
+            Event::Key(KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: KeyModifiers::CONTROL,
+            }) => break,
+            _ => (),
+        }
+    }
+
+    stdout
+        .queue(Show).unwrap()
+        .queue(LeaveAlternateScreen).unwrap();
+
+    stdout.flush().unwrap();
+
+    disable_raw_mode().unwrap();
+}
+
+fn draw(stdout: &mut Stdout, width: u16, height: u16) -> Result<(), Error> {
+    stdout
+        .queue(MoveTo(0, 0)).unwrap()
+        .queue(PrintStyledContent("❯ ".green())).unwrap()
+        .queue(Print(format!("{:width$}", "", width=width as usize))).unwrap();
+
+    stdout.flush().unwrap();
+
+    Ok(())
 }
